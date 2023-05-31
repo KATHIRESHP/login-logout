0ec60096140b4c6bb7358ed1e261df2e4fe1c435
KATHIRESHP <kathireshpalanisamy055@gmail.com>
2023-05-31 14:45:20 +0530

    144531052023

--- client/inout/src/Components/Login/Password_Reset.jsx
+++ client/inout/src/Components/Login/Password_Reset.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 const Password_Reset = () => {
+
+  const emailRef = useRef(null);
   return (
     <div>
-      <div className='container h-screen w-screen bg-black'>
-          
+      <div className='vh-100 wh-100 d-flex justify-content-center align-items-center bg-light'>
+        <div className='bg-success col-md-9 col-sm-11 col-lg-4 offset-sm-0 p-sm-5 p-md-5 rounded-4 shadow-lg'>
+          <div className="form-floating">
+            <input ref={emailRef} type="email" className="form-control" id="floatingEmail" placeholder="n" />
+            <label htmlFor="floatingEmail">Email</label>
+          </div>
+          <center><button className='btn btn-danger my-4'>Reset</button></center>
+        </div>
       </div>
     </div>
   )
